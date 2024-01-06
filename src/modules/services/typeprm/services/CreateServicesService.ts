6eaf4ea04f977317a9ad548bf19805faa2118ad3
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2024-01-06 12:47:59 -0300

    Add service CreateService

---
+++ src/modules/services/typeprm/services/CreateServicesService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from "typeorm";
+import Service from "../entities/Service";
+import { serviceRepository } from "../repositories/ServicesRepository";
+import AppError from "@shared/errors/AppError";
+
+interface IRequest{
+  name: string;
+  description: string;
+  price: number;
+  duration: number;
+}
+
+class CreateServicesService {
+  public async execute({name, description, price, duration}: IRequest): Promise<Service>{
+    const servicesReposity = getCustomRepository(serviceRepository);
+    const serviceExists = await servicesReposity.findByName(name);
+
+    if (serviceExists) {
+      throw new AppError('There is already one service with this name')
+    }
+
+    const service = servicesReposity.create({
+      name,
+      description,
+      price,
+      duration
+    });
+
+    await servicesReposity.save(service);
+
+    return service;
+  }
+}
+
+export default CreateServicesService;
